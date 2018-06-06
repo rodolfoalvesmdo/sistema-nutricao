51a49baf03787dca18375d22a5581110fbffb981
Rodolfo Alves <rodolfoalvesmdo@gmail.com>
2018-06-06 03:50:06 -0300

    Config para servidor de produção e favicon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    < link rel="shortcut icon" href="{{ asset('favicon.ico') }}" >
     
 </head>
 <body>
